a62a52ec8fb802611b47da93a7a1f6963be62ae1
gergolesk <74426622+gergolesk@users.noreply.github.com>
2023-03-04 17:51:05 +0300

    Update remove_duplicates.cpp

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -4,26 +4,28 @@
 using namespace std;
 
 void RemoveDuplicates(SearchServer& search_server) {
-    map<set<string>, set<int>> words_in_documents;
+    set<set<string>> docs;
+    vector<int> found_duplicates;
 
-    for (auto it = search_server.begin(); it != search_server.end(); ++it) {
+    for (int id : search_server) {
+        auto& freqs = search_server.GetWordFrequencies(id);
         set<string> words_in_document;
-        for (const auto& [word, freq] : search_server.GetWordFrequencies(*it)) {
-            words_in_document.insert(word);
+
+        transform(freqs.begin(), freqs.end(), inserter(words_in_document, words_in_document.begin()),
+            [](auto p) {
+                return p.first;
+            });
+
+        if (docs.count(words_in_document) > 0) {
+            cout << "Found duplicate document id " << id << endl;
+            found_duplicates.push_back(id);
         }
-        words_in_documents[words_in_document].insert(*it);
+        else {
+            docs.insert(words_in_document);
+        }
     }
 
-    for (const auto& [wrds, ids] : words_in_documents) {
-        if (ids.size() == 1) {
-            continue;
-        }
-
-        auto it = ids.begin();
-        ++it;
-        for (; it != ids.end(); ++it) {
-            cout << "Found duplicate document id "s << *it << endl;
-            search_server.RemoveDocument(*it);
-        }
+    for (int id : found_duplicates) {
+        search_server.RemoveDocument(id);
     }
 }
